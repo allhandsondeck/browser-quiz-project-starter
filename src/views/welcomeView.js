0ae58defd50996280fb79d55eb2d9496ca9c1dfd
trebektas <trensarbektas@gmail.com>
2022-10-12 20:24:41 +0200

    complete Landing Page changes

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -9,10 +9,10 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1 id="welcome-h1">Welcome</h1>
-    <div id="rules-of-the-quiz">
-      <h3>Rules of the quiz</h3>
-      <ul id="list-of-rules">
+    <h1 class="welcome-h1">Welcome</h1>
+    <div class="rules-of-the-quiz">
+      <h3 class="rules-h3">Rules of the quiz</h3>
+      <ul class="list-of-rules">
         <li>The quiz consists of <span class="red-color">10 questions</span> in total.</li>
         <li>You will have <span class="red-color">15 seconds</span> for each question!</li>
         <li>You can't select two answers</li>
