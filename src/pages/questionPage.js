c57d760fb9876ae468e636cee71bb5e6009439c4
Riam Alali <48811666+RiamAlali@users.noreply.github.com>
2022-10-14 23:01:58 +0200

    Showing-the-progress-in-terms-of-question-number

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -11,6 +11,7 @@ import { quizData } from '../data.js';
 import { initCounter } from '../views/countdownView.js';
 import { countdownInterval } from '../views/countdownView.js';
 import { resultPage } from '../pages/resultPage.js';
+import { initQuestionProgress } from '../views/progressView.js';
 
 export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -28,7 +29,7 @@ export const initQuestionPage = () => {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
   }
-
+  initQuestionProgress(); 
   document
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion);
--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,9 +1,10 @@
 'use strict';
 
-import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
+import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID, PROGRESS_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 import { initCounter } from '../views/countdownView.js';
+import { initQuestionProgress } from '../views/progressView.js';
 
 export const initWelcomePage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -20,4 +21,5 @@ export const initWelcomePage = () => {
 const startQuiz = () => {
   initQuestionPage();
   initCounter();
+  initQuestionProgress();
 };
